4676659174a969dc73e8c60fbf767daff2a69c28
UBC Student <bjiang10@students.cs.ubc.ca>
2023-12-26 13:06:48 -0800

    completed email login2

--- app/src/main/java/com/example/pet/MainActivity.kt
+++ app/src/main/java/com/example/pet/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.pet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
